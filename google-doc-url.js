1b6657671f1217ccc4b3a4440990b2617556550d
Razelos Konstantinos <44059820+razeloskon@users.noreply.github.com>
2018-11-07 13:20:56 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/14jojNrxAZSh1kd0hVcrnXdDj29Liq60a1R6CoEXlr3Y/edit#gid=0';
